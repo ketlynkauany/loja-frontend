6bd981dd5fcc8db61e58db2f7538c44251c5fc28
ketlynkauany <ketlynlove13@gmail.com>
2026-09-14 23:49:46 -0300

    implementa CarrinhoService

--- src/app/carrinho-service.service.ts
+++ src/app/carrinho-service.service.ts
@@ -1,9 +1,81 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+
+export type Produto = {
+  id: number;
+  nome: string;
+  preco: number;
+};
+
+/**
+ * Composto por um produto e a quantidade desse produto
+ * que pode ser adicionado ao carrinho
+ */
+export type Item = {
+  id: number;
+  produto: Produto;
+  quantidade: number;
+};
 
 @Injectable({
   providedIn: 'root'
 })
-export class CarrinhoServiceService {
+export class CarrinhoService {
 
-  constructor() { }
+  itens = signal<Item[]>([]);
+
+  adicionarItem(produto: Produto) {
+    this.itens.update(itens => {
+      const item = itens.find(item => item.produto.id === produto.id);
+
+      if (item) {
+        return itens.map(item =>
+          item.produto.id === produto.id
+            ? { ...item, quantidade: item.quantidade + 1 }
+            : item
+        );
+      }
+
+      return [
+        ...itens,
+        {
+          id: produto.id,
+          produto: produto,
+          quantidade: 1
+        }
+      ];
+    });
+  }
+
+  aumentarQuantidade(id: number) {
+    this.itens.update(itens =>
+      itens.map(item =>
+        item.id === id
+          ? { ...item, quantidade: item.quantidade + 1 }
+          : item
+      )
+    );
+  }
+
+  diminuirQuantidade(id: number) {
+    this.itens.update(itens =>
+      itens.map(item =>
+        item.id === id && item.quantidade > 1
+          ? { ...item, quantidade: item.quantidade - 1 }
+          : item
+      )
+    );
+  }
+
+  removerItem(id: number) {
+    this.itens.update(itens =>
+      itens.filter(item => item.id !== id)
+    );
+  }
+
+  obterTotal() {
+    return this.itens().reduce(
+      (total, item) => total + item.produto.preco * item.quantidade,
+      0
+    );
+  }
 }
